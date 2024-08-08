6711a86969fedcb37c35519da4a1ea0675ce067d
Pedro S <p.sousa.silva88@gmail.com>
2024-08-08 15:35:43 +0100

    updated db context

--- src/CarAuction.Infrastructure/DependencyInjection.cs
+++ src/CarAuction.Infrastructure/DependencyInjection.cs
@@ -10,17 +10,8 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDbContext<CarAuctionContext>(options =>
-        {
-            options.UseInMemoryDatabase("InMemoryDb");
-            //options.EnableDetailedErrors();
-            //options.EnableSensitiveDataLogging();
-            //options.UseSqlServer(configuration.GetConnectionString("SqlServer"),
-            //    sqlOptions =>
-            //    {
-            //        sqlOptions.EnableRetryOnFailure(5, TimeSpan.FromSeconds(15), null);
-            //    });
-        });
+        services.AddDbContext<CarAuctionContext>(options => 
+            options.UseInMemoryDatabase("InMemoryDb"));
 
         services.AddScoped<ICarAuctionContext>(provider =>
             provider.GetRequiredService<CarAuctionContext>());
